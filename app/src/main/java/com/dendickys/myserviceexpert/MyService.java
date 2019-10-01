424bba048378bb87b49640dae63684c2de5da903
dendickys <febri.unquad2014@gmail.com>
2019-10-01 21:29:58 +0700

    membuat sebuah background process seperti pada modul sebelumnya untuk mematikan/menghentikan service yang telah dijalankan dengan memanggil metode stopSelf() setelah 3 detik.

--- app/src/main/java/com/dendickys/myserviceexpert/MyService.java
+++ app/src/main/java/com/dendickys/myserviceexpert/MyService.java
@@ -2,12 +2,16 @@ package com.dendickys.myserviceexpert;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
-public class MyService extends Service {
+import java.lang.ref.WeakReference;
 
-    public static final String TAG = "MyService";
+//langkah 2 : implement interface dan implement method preAsync dan postAsync
+public class MyService extends Service implements DummyAsyncCallback {
+
+    public static final String TAG = MyService.class.getSimpleName();
 
     public MyService() {
     }
@@ -20,7 +24,72 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, "onStartCommand: ");
+        Log.d(TAG, "Service dijalankan...");
+
+        //langkah 4 : inisialisasi dan jalankan AsyncTask
+        DummyAsync dummyAsync = new DummyAsync(this);
+        dummyAsync.execute();
+
         return START_STICKY;
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    //langkah 5 : tambahkan aksi di callback
+    @Override
+    public void preAsync() {
+        Log.d(TAG, "preAsync: Mulai.....");
+    }
+
+    @Override
+    public void postAsync() {
+        Log.d(TAG, "postAsync: Selesai.....");
+        stopSelf();
+    }
+
+    //langkah 3 : buat AsyncTask dan WeakReference
+    private static class DummyAsync extends AsyncTask<Void, Void, Void> {
+
+        private final WeakReference<DummyAsyncCallback> callback;
+
+        DummyAsync(DummyAsyncCallback callback) {
+            this.callback = new WeakReference<>(callback);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Log.d(TAG, "onPreExecute: ");
+            callback.get().preAsync();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.d(TAG, "doInBackground: ");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Log.d(TAG, "onPostExecute: ");
+            callback.get().postAsync();
+        }
+    }
+
+}
+
+//langkah 1 : buat interface
+interface DummyAsyncCallback {
+    void preAsync();
+    void postAsync();
 }
