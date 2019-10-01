3d754565893d1481e6e8f5258e1511f925a982c6
dendickys <febri.unquad2014@gmail.com>
2019-10-01 21:13:57 +0700

    Add start_service

--- app/src/main/java/com/dendickys/myserviceexpert/MainActivity.java
+++ app/src/main/java/com/dendickys/myserviceexpert/MainActivity.java
@@ -2,6 +2,7 @@ package com.dendickys.myserviceexpert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,7 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_start_service:
-
+                Intent mStartServiceIntent = new Intent(MainActivity.this, MyService.class);
+                startService(mStartServiceIntent);
                 break;
             case R.id.btn_start_intent_service:
 
