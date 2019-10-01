e2cbaeb67aff3dd4df9c23e344c640b419964ce1
dendickys <febri.unquad2014@gmail.com>
2019-10-02 00:09:46 +0700

    Add MyIntentService

---
+++ app/src/main/java/com/dendickys/myserviceexpert/MyIntentService.java
@@ -0,0 +1,37 @@
+package com.dendickys.myserviceexpert;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MyIntentService extends IntentService {
+
+    public static String EXTRA_DURATION = "extra_duration";
+    public static final String TAG = MyIntentService.class.getSimpleName();
+
+    public MyIntentService() {
+        super("Custom Intent Service");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            long duration = intent.getLongExtra(EXTRA_DURATION, 0);
+            try {
+                Thread.sleep(duration);
+                Log.d(TAG, "onHandleIntent: Selesai.....");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
+}
